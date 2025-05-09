eb7ce14802cd54c0ea47477a92681ffca90da966
AlanAruFlores <alanaruquipa242@gmail.com>
2025-05-08 23:17:30 -0300

    minor: making minor changes

--- src/main/java/com/example/blockchainremito/BlockchainRemitoPocApplication.java
+++ src/main/java/com/example/blockchainremito/BlockchainRemitoPocApplication.java
@@ -19,22 +19,5 @@ public class BlockchainRemitoPocApplication {
         SpringApplication.run(BlockchainRemitoPocApplication.class, args);
     }
 
-    /* 
-    @Bean
-    CommandLineRunner init(RemitoService remitoService) {
-        return args -> {
-            Remito remito = Remito.builder()
-                .fecha(LocalDate.now())
-                .empresaSolicitante("Empresa Solicitante")
-                .empresaProveedora("Empresa Proveedora")
-                .descripcionTrabajo("Descripción del trabajo")
-                .tecnicoAsignado("Técnico Asignado")
-                .build();
-
-
-            CompletableFuture<Remito> future = remitoService.guardarRemito(remito);
-            System.out.println("Remito guardado en la blockchain: " + future.get()); // Esto hará que el hilo principal espere a que se complete el proceso asíncrono       
-        };
-    }
-    */
+    
 }
